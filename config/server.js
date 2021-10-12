3dfa2b259abfdfa357345e9c4557c343e7c7da92
NA1000APP-ROMUSNP1 <DL-1000-RPO-DEV-ROMUS DL1000RPODEVROMUS@monsanto.com>
2021-10-11 23:24:01 -0500

    initial backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1339),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b27d1918edee51b4aba375ca418f1f04'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c1650986-122d-4707-8df7-d3cb6e96c3c3'
+};
